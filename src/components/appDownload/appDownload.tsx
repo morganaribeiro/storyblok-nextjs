97176a2731fa4da3a3aa74864a1bf690614eed6c
Morgana Oliveira <morgana.ifce.2019@gmail.com>
2025-07-29 09:07:59 -0300

    Revert "feat(appDownload): test component"
    This reverts commit b3d6244ae05114f104d5ecc0f29342456a16df6e.

--- src/components/appDownload/appDownload.tsx
+++ src/components/appDownload/appDownload.tsx
@@ -1,54 +1,8 @@
-import { storyblokEditable } from "@storyblok/react";
 
-type Blok = {
-  _uid: string;
-  title: string;
-  text: string;
-  active: boolean;
-  contact: string;
-  qr_code: {
-    title: string;
-    alt: string;
-    filename: string;
-  }
-  link_ios: {
-    url: string;
-    target: string;
-  }
-  app_store: {
-    title: string;
-    alt: string;
-    filename: string;
-  }
-};
-
-type PropsBlok = {
-  blok: Blok;
-};
-
-export const appDownload = ({ blok }: PropsBlok) => {
+export const appDownload = (props: any) => {
   return (
-    <section {...storyblokEditable(blok)} key={blok._uid}>
-      <>Conteúdo do bloco appDownload:</>
-      <br/>
-      {blok.title}
-      <br/>
-      {blok.text}
-      <br/>
-      {blok.active}
-      <br/>
-      {blok.contact}
-      <br/>
-      {blok.qr_code.title}
-      {blok.qr_code.alt}
-      {blok.qr_code.filename}
-      <br/>
-      {blok.link_ios.url}
-      {blok.link_ios.target}
-      <br/>
-      {blok.app_store.title}
-      {blok.app_store.alt}
-      {blok.app_store.filename}
-    </section>
+    <div>
+      {JSON.stringify(props, null, 2)}
+    </div>
   );
 };
